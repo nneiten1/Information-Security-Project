b8a2905dc31fa87dcdc55774f6573c3fb537c86d
Nick Neitenbach <nneiten1@kent.edu>
2025-04-07 10:27:28 -0400

    database created

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+
+app = Flask(__name__)
+
+def register_user(username, password):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+        conn.commit()
+        print("User Registered")
+    except sqlite3.IntegrityError:
+        print("Username already exists")
+    finally:
+        conn.close()
+
+
+def login_user(username, password):
+    conn = sqlite3.connect('user.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
+    user = cursor.fetchone
+
+    conn.close()
+    return user is not None
+
+
+@app.route('/')
+def home():
+        return redirect(url_for('login'))
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        if register_user(username, password):
+            return "Registered successfully! <a href='/login'>Login</a>"
+        else:
+            return "Username is already in use."
+    return render_template('register.html')
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if login_user(username, password):
+            return f"Welcome, {username}!"
+        else:
+            return "Invalid login."
+    
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
